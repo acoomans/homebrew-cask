eabcf6c094656dc189e272aae06e0bbd23bc2cd6
Arnaud Coomans <arnaud.coomans@gmail.com>
2017-08-22 00:21:27 -0400

    Added Cask for yubiswitch 0.12
    Added a cask for the Yubiswitch application available at https://github.com/pallotron/yubiswitch.
    OSX status bar application to enable/disable Yubikey Nano

---
+++ Casks/yubiswitch.rb
@@ -0,0 +1,10 @@
+cask 'yubiswitch' do
+  version '0.12'
+  sha256 '691ff7017c000211587217c60a6a9f352b3cb8d9ee2ce099f01d096d794a01ff'
+
+  url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
+  name 'yubiswitch'
+  homepage 'https://github.com/pallotron/yubiswitch'
+
+  app 'yubiswitch.app'
+end
